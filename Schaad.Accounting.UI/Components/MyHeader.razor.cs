38a1fb65c5adcdf3e6066fe5724d370e98242d8f
Claudio Schaad <c.schaad@pog.ch>
2025-05-04 21:49:01 +0200

    Add year and mandator dropdown

---
+++ Schaad.Accounting.UI/Components/MyHeader.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Schaad.Accounting.Interfaces;
+
+namespace Schaad.Accounting.UI.Components;
+
+public partial class MyHeader : ComponentBase
+{
+    [Inject]
+    private ISettingsService settingsService { get; set; } = null!;
+    
+    private List<int> years = new ();
+    private int selectedYear;
+    
+    protected override Task OnInitializedAsync()  
+    {  
+        for (var year = DateTime.Now.Year; year >= 2015; year--)
+        {
+            years.Add(year);
+        }
+
+        selectedYear = settingsService.GetYear();
+        
+        return base.OnInitializedAsync();  
+    }
+    
+    public void YearChanged(string obj)
+    {
+        settingsService.SetYear(int.Parse(obj));
+    }
+
+}
