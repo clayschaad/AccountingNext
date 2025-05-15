a45495c2960fe7b92a27d0bf2ae6dac0ed9c2125
Claudio Schaad <c.schaad@pog.ch>
2025-05-15 22:01:48 +0200

    Add autocomplete for account

--- Schaad.Accounting.UI/Components/Pages/Controls/AccountSelector.razor.cs
+++ Schaad.Accounting.UI/Components/Pages/Controls/AccountSelector.razor.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 using Schaad.Accounting.Models;
 
-namespace Schaad.Accounting.UI.Components.Pages.Controls;
+namespace Schaad.Accounting.UI.Components.Pages.Controls; 
 
 public partial class AccountSelector : ComponentBase
 {
@@ -10,10 +11,25 @@ public partial class AccountSelector : ComponentBase
     [Parameter] public required IReadOnlyList<Account> Accounts { get; set; }
 
     [Parameter] public EventCallback<string> AccountIdChanged { get; set; }
+    
+    private Account? SelectedAccount { get; set; }
 
-    private async Task OnInputChanged(ChangeEventArgs e)
+    protected override void OnInitialized()
     {
-        AccountId = e.Value?.ToString();
+        SelectedAccount = Accounts.FirstOrDefault(account => account.Id == AccountId);
+    }
+    
+    private async Task OnSearchAsync(OptionsSearchEventArgs<Account> e)
+    {
+        e.Items = Accounts.Where(i => i.Name.Contains(e.Text, StringComparison.OrdinalIgnoreCase))
+            .OrderBy(i => i.Name);
+        await Task.CompletedTask;
+    }
+    
+    public async Task OnSelectedChanged(IEnumerable<Account>? s)
+    {
+        SelectedAccount = s?.FirstOrDefault();
+        AccountId = SelectedAccount?.Id;
         await AccountIdChanged.InvokeAsync(AccountId);
     }
 }
